30c56b9c7ff1c1034cd3cf74f1ae363c1db877d8
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-10-24 22:54:42 +0300

    Первичная настройка проекта

---
+++ users/management/commands/csu.py
@@ -0,0 +1,20 @@
+import os
+
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email=os.getenv('ADMIN_EMAIL'),
+            first_name=os.getenv('ADMIN_NAME'),
+            last_name=os.getenv('ADMIN_LAST_NAME'),
+            is_staff=True,
+            is_superuser=True
+        )
+
+        user.set_password(os.getenv('ADMIN_PASSWORD'))
+        user.save()
---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
