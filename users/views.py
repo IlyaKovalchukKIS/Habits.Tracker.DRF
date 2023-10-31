f35852f5fe9be39af22033790528f40ceedbc0be
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-11-01 01:12:07 +0300

    Написан сериализатор для создания пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from pprint import pprint
 
-# Create your views here.
+from django.shortcuts import render
+from django_celery_beat.models import PeriodicTask
+from rest_framework import viewsets
+
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
