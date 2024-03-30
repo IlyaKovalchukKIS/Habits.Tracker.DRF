a718e5a7751fe37eae1c7a39321bc3a502144eec
User <mrlivedance@vk.com>
2024-03-31 02:42:19 +0300

    написаны тесты для пользователей и привычек

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from users.tests import UserRegistrationMixin
 
-# Create your tests here.
+
+class HabitsTestCase(UserRegistrationMixin, APITestCase):
+    def setUp(self):
+        super().setUp()
+        self.headers = {"Authorization": f"Bearer {self.user_token.json()['access']}"}
+        self.data = {
+            "place": 'test place',
+            'time': '11:22',
+            'action': 'test action',
+            'frequency': '1',
+            'award': 'test award'
+        }
+        self.habit = self.client.post(
+            '/habits/create/',
+            data=self.data,
+            headers=self.headers
+        )
+
+    def test_create_habit(self):
+        """ Тестирование создания привычки """
+
+        self.assertEqual(
+            self.habit.status_code, status.HTTP_201_CREATED
+        )
+
+    def test_update_habit(self):
+        """ Тестирование изменения привычки """
+        self.data['place'] = 'test place update'
+        response = self.client.put(
+            f'/habits/update/{self.habit.json()["id"]}/',
+            data=self.data,
+            headers=self.headers
+        )
+
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+
+    def test_list_habits(self):
+        """ Тестирования вывода списка привычек """
+
+        response = self.client.get(
+            '/habits/list/',
+            headers=self.headers
+        )
+
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json()['count'], 1
+        )
+
+    def test_destroy_habit(self):
+        """ Тестирование удаления привычки """
+
+        response = self.client.delete(
+            f'/habits/destroy/{self.habit.json()["id"]}/',
+            data=self.data,
+            headers=self.headers
+        )
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT
+        )
+
+        response = self.client.delete(
+            f'/habits/destroy/{self.habit.json()["id"]}/',
+            data=self.data
+        )
+        self.assertEqual(
+            response.status_code, status.HTTP_401_UNAUTHORIZED
+        )
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,60 @@
-# from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class UserRegistrationMixin(APITestCase):
+    def setUp(self):
+        self.data = {
+            "email": 'test_mail@dot.com',
+            'password': 'test_password'
+        }
+        self.user = self.client.post(
+            '/users/user/', data=self.data
+        )
+        self.user_token = self.client.post(
+            '/users/token/', data=self.data
+        )
+
+
+class UserTestCase(UserRegistrationMixin, APITestCase):
+    def test_create_user(self):
+        """ Тестирование регистрации пользователя """
+
+        self.assertEqual(
+            self.user.status_code,
+            status.HTTP_201_CREATED
+        )
+
+    def test_get_token_user(self):
+        """ Получение токена пользователем """
+
+        self.assertEqual(
+            self.user_token.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_user_update(self):
+        """ Изменение данных пользователя """
+
+        response = self.client.put(
+            f"/users/user/{self.user.json()["id"]}/",
+            data={"password": 'test_update_password',
+                  "email": 'test_mail@dot.com'},
+            headers={"Authorization": f"Bearer {self.user_token.json()['access']}"}
+        )
+        self.assertEqual(
+            response.json()['password'], 'test_update_password'
+        )
+
+    def test_user_delete(self):
+        """ Удаления пользователя """
+
+        user_id = self.user.json()["id"]
+        access_token = self.user_token.json()['access']
+
+        response = self.client.delete(
+            f'/users/user/{user_id}/', headers={"Authorization": f"Bearer {access_token}"}
+        )
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT
+        )
